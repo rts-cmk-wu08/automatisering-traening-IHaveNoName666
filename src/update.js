3c1f2e7d49e6e75bd0ba72a6124cc70b6689c3d5
FunkaZan <U26895@rts-365.dk>
2022-10-26 08:09:35 +0200

    automatisering

--- src/update.js
+++ src/update.js
@@ -1,4 +1,16 @@
+import UpdateForm from "./component/UpdateForm.js"
+
+let params = new URLSearchParams(window.location.search)
+let id = params.get("id");
+
+console.log(id)
+
 let element = document.querySelector(".wrapper")
+
 let main = document.createElement("main")
+
+main.append(UpdateForm(id))
+
+
 
 element.append(main)
